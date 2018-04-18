6d16faf0e355a36569a1e80990b045cb4e947246
Alexander Alvarez <alexander.gabriel.alvarez@Gmail.com>
2018-04-18 11:38:12 -0400

    Add files via upload

---
+++ XML/login.php
@@ -0,0 +1,62 @@
+ <?php  
+ 
+ 
+	if (isset($_POST['username']) && isset($_POST['password'])) 
+	{
+		
+		
+ 
+		// receiving the post params
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		
+		$password = md5($password);
+		
+		
+		static $db;
+
+			if(!isset($db)) 
+			{
+				$db = mysqli_connect("localhost", "webalex", "vr8aTp573L", "webalex_SwoleAF");
+			}
+
+			if($db === false) 
+			{
+				return mysqli_connect_error();
+			}
+			
+			
+			$response = array("error" => FALSE);
+			
+			
+		
+			$sql = "CALL Login('" . $username . "','" . $password . "')";
+			
+			$result = mysqli_query($db, $sql);
+			
+
+			 
+      		if($result->num_rows > 0) 
+
+			{
+				$response["error"] = FALSE;
+				//$row = $result->fetch_assoc();
+				$id = $row["UserID"];
+				//	echo $id;
+				session_start();
+				$_SESSION['user'] = $id;
+				
+				echo json_encode($response);
+        		
+			} 
+			
+			else 
+			{
+				
+				 $response["error"] = TRUE; 
+				 $response["error_msg"] = "Invalid Login. Please Try Again.";
+				 echo json_encode($response);
+			}
+	}
+ ?>  
+ 
